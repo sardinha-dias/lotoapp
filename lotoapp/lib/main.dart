89f911a3db5edb4a5c419f4b9abc86bd57124033
sardinha-dias <sardinha.dias@gmail.com>
2022-08-08 00:09:32 -0300

    implementa numeros aleatórios

--- lotoapp/lib/main.dart
+++ lotoapp/lib/main.dart
@@ -29,6 +29,38 @@ class LotoApp extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 13, 228, 24),
       ),
+      body: BolasEscolhidas(),
     );
   }
 }
+
+class BolasEscolhidas extends StatefulWidget {
+  const BolasEscolhidas({Key? key}) : super(key: key);
+
+  @override
+  State<BolasEscolhidas> createState() => _BolasEscolhidasState();
+}
+
+class _BolasEscolhidasState extends State<BolasEscolhidas> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+      ),
+      itemCount: 50,
+      itemBuilder: (BuildContext context, int index) {
+        return CircleAvatar(
+          backgroundColor: Colors.white,
+          radius: 5.0,
+        );
+      },
+    );
+  }
+}
+
+//class Bolas {
+
+
+
+
